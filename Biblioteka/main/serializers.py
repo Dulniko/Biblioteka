23990f3756d4562f8b4295ab2b63263004323096
Dulniko <jakub.dulnik03@gmail.com>
2023-02-18 18:05:23 +0100

    dodawanie po int id

--- Biblioteka/main/serializers.py
+++ Biblioteka/main/serializers.py
@@ -21,17 +21,35 @@ GENRE_CHOICES = (
 class BookSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
-    author = AuthorSerializer()
+    author_id = serializers.IntegerField()
     isbn = serializers.CharField(max_length=13)
     genre = serializers.ChoiceField(choices=GENRE_CHOICES)
     publication_date = serializers.DateField()
     publisher = serializers.CharField(max_length=255)
 
+    def validate(self, data):
+        author_id = data.get('author_id')
+        if author_id and not Author.objects.filter(id=author_id).exists():
+            raise serializers.ValidationError('Invalid author_id.')
+        return data
+
+    def create(self, validated_data):
+        return Book.objects.create(**validated_data)
+
 
 
 class LoanSerializer(serializers.Serializer):
     id = serializers.IntegerField()
-    book = BookSerializer()
+    book_id = serializers.IntegerField()
     borrower = serializers.CharField(max_length=255)
     loan_date = serializers.DateField()
     return_date = serializers.DateField()
+
+    def validate(self, data):
+        book_id = data.get('book_id')
+        if book_id and not Book.objects.filter(id=book_id).exists():
+            raise serializers.ValidationError('Invalid author_id.')
+        return data
+
+    def create(self, validated_data):
+        return Loan.objects.create(**validated_data)
