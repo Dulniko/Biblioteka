55c06153e4c467430e1ff9e8d3cc5c00d05b834b
Dulniko <jakub.dulnik03@gmail.com>
2023-02-19 19:15:44 +0100

    urls / change to router

--- Biblioteka/main/urls.py
+++ Biblioteka/main/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path
-from .views import BookList, BookDetail, AuthorList, AuthorDetail, LoanList, LoanDetail
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet, AuthorViewSet, LoanViewSet
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+router.register(r'authors', AuthorViewSet)
+router.register(r'loans', LoanViewSet)
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetail.as_view(), name='book-detail'),
-    path('author/', AuthorList.as_view(), name='author-list'),
-    path('author/<int:pk>/', AuthorDetail.as_view(), name='author-detail'),
-    path('loan/', LoanList.as_view(), name='loan-list'),
-    path('loan/<int:pk>/', LoanDetail.as_view(), name='loan-detail'),
+    path('', include(router.urls)),
 ]
--- Biblioteka/main/views.py
+++ Biblioteka/main/views.py
@@ -1,58 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import Http404
-from rest_framework import generics
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from .serializers import BookSerializer, AuthorSerializer, LoanSerializer
 from .models import Book, Author, Loan
+from rest_framework import viewsets
 
-def mainpage(request):
-    return HttpResponse("Hello, World!")
-
-
-class BookList(generics.ListCreateAPIView):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-class AuthorList(APIView):
-    def get(self, request):
-        authors = Author.objects.all()
-        serializer = AuthorSerializer(authors, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = AuthorSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class AuthorDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Author.objects.get(pk=pk)
-        except Author.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        author = self.get_object(pk)
-        serializer = AuthorSerializer(author)
-        return Response(serializer.data)
-
-class AuthorDetail(generics.RetrieveUpdateDestroyAPIView):
+class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
 
-class LoanList(generics.ListCreateAPIView):
-    queryset = Loan.objects.all()
-    serializer_class = LoanSerializer
-
-class LoanDetail(generics.RetrieveUpdateDestroyAPIView):
+class LoanViewSet(viewsets.ModelViewSet):
     queryset = Loan.objects.all()
     serializer_class = LoanSerializer
